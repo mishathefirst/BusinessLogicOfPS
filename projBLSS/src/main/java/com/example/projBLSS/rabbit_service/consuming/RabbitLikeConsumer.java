d31c37d92e757ba4cff50d03c7b52ac424aa600a
whatislove118 <oleg.pochikalin@mail.ru>
2021-05-21 14:54:32 +0300

    add_websockets

--- projBLSS/src/main/java/com/example/projBLSS/rabbit_service/consuming/RabbitLikeConsumer.java
+++ projBLSS/src/main/java/com/example/projBLSS/rabbit_service/consuming/RabbitLikeConsumer.java
@@ -2,8 +2,10 @@ package com.example.projBLSS.rabbit_service.consuming;
 
 
 import com.example.projBLSS.main_server.dto.PictureToStatsServerDTO;
+import com.example.projBLSS.main_server.dto.ResponseMessageDTO;
 import com.example.projBLSS.main_server.exceptions.PictureNotFoundException;
 import com.example.projBLSS.main_server.service.PictureService;
+import com.example.projBLSS.rabbit_service.websocket.WebSocketService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +21,21 @@ public class RabbitLikeConsumer {
     private ConsumerService consumerService;
 
 
+    @Autowired
+    private WebSocketService webSocketService;
+
+
     @RabbitHandler
-    public void receive(PictureToStatsServerDTO picture) throws PictureNotFoundException {
-        System.out.println(" [x] Received '" + picture.getId() + "'");
-        this.pictureService.incrementLikePicture(picture.getId(), picture.getCountLikesToAdd());
-        if(!consumerService.checkLikeGoal(picture.getId())){
-            System.out.println("YEEEP");
+    public void receive(PictureToStatsServerDTO picture){
+        ResponseMessageDTO message = new ResponseMessageDTO();
+        try {
+            this.pictureService.incrementLikePicture(picture.getId(), picture.getCountLikesToAdd());
+            if(!consumerService.checkLikeGoal(picture.getId())){
+                message.setAnswer("Лайк был установлен");
+            }
+            webSocketService.send(message, pictureService.getPicture(picture.getId()).getUserID());
+        }catch (PictureNotFoundException e){
+            message.setAnswer(e.getErrMessage());
         }
     }
 
