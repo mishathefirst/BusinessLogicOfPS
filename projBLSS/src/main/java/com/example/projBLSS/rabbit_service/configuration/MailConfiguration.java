0c5fb30388e18cd199e0fccf355a303facec9d4b
whatislove118 <oleg.pochikalin@mail.ru>
2021-06-01 20:13:27 +0300

    change_password

--- projBLSS/src/main/java/com/example/projBLSS/rabbit_service/configuration/MailConfiguration.java
+++ projBLSS/src/main/java/com/example/projBLSS/rabbit_service/configuration/MailConfiguration.java
@@ -18,8 +18,8 @@ public class MailConfiguration {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("pochikalin@gmail.com");
-        mailSender.setPassword("hokage9916");
+        mailSender.setUsername("");
+        mailSender.setPassword("");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
